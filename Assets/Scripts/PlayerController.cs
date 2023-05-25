0446c3865f466cf9e130b1953e3499ac4b73dc97
Kübra <k.yildirim1499@gmail.com>
2023-05-25 09:54:59 +0300

    kübra

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float rotateBound = 0.5f;
     [SerializeField] float rotateValue = 0;
     [SerializeField] float temp = 0;
+    private float minSize = 0.25f;
     private bool canMove = false;
     private Touch theTouch;
     #region 
@@ -18,10 +19,10 @@ public class PlayerController : MonoBehaviour
     public float timeThreshold = 2f; // Minimum ayný deðerde kalma süresi
     private float stableTime = 0f;
     #endregion
-    [SerializeField] private GameObject generalBody;
+    [SerializeField] private List<GameObject> bodyParts;
     [SerializeField] private GameObject upBody;
     [SerializeField] private GameObject torso;
-    [SerializeField] private GameObject head;
+    [SerializeField] private GameObject hips;
 
     [SerializeField] private GameObject bodyPiece;
     private void FixedUpdate()
@@ -86,26 +87,55 @@ public class PlayerController : MonoBehaviour
 
     public void GetFatOrSlim(float value)
     {
-        value = value / 150;
-        generalBody.transform.DOScale(generalBody.transform.localScale + new Vector3(value, 0, value), 1f);
+        value = value / 100;
+        if (bodyParts[0].transform.localScale.x  ==  minSize)
+        {
+
+            foreach (GameObject obj in bodyParts)
+            {
+                obj.transform.DOScale(new Vector3(0,0,0), 1f);
+            }
+            hips.transform.DOScale( new Vector3(0,0,0), 1f).OnComplete(()=>{ print("Game Over"); });
+
+        }
+        else if (bodyParts[0].transform.localScale.x + value < minSize)
+        {
+            foreach (GameObject obj in bodyParts)
+            {
+                obj.transform.DOScale(new Vector3(minSize, 0, minSize), 1f);
+            }
+            hips.transform.DOScale(hips.transform.localScale + new Vector3(minSize * 0.5f, minSize * 0.5f, minSize), 1f);
+        }
+        else
+        {
+            foreach (GameObject obj in bodyParts)
+            {
+                obj.transform.DOScale(obj.transform.localScale + new Vector3(value, 0, value), 1f);
+            }
+            hips.transform.DOScale(hips.transform.localScale + new Vector3(value * 0.5f, value * 0.5f, value), 1f);
+        }
     }
 
     public void GetTallOrShort(float value)
     {
         value = value / 100;
-        float temp = (value) / 0.5f * 0.2f;
-        print(temp);
-        torso.transform.DOScale(new Vector3(torso.transform.localScale.x, torso.transform.localScale.y + value, torso.transform.localScale.z), 1f);
-        head.transform.DOMoveY(head.transform.position.y + temp, 1f);
-        upBody.transform.DOMoveY(upBody.transform.position.y + temp, 1f);
+        if (torso.transform.position.y + value <= 0)
+        {
+            GetFatOrSlim(value * 100);
+        }
+        else
+        {
+            torso.transform.DOMoveY(torso.transform.position.y + value, 1f);
+            upBody.transform.DOMoveY(upBody.transform.position.y + value, 1f);
+        }
     }
 
     public void Hit(Transform hitPoint)
     {
         GetTallOrShort(-50);
-        Vector3 spawnPoint = new Vector3(transform.position.x, hitPoint.position.y, hitPoint.position.z - (generalBody.transform.localScale.x * 0.2f * 2));
+        Vector3 spawnPoint = new Vector3(transform.position.x, hitPoint.position.y, transform.position.z);
         GameObject bodyPieceClone = Instantiate(bodyPiece, spawnPoint, Quaternion.identity);
-        bodyPieceClone.GetComponent<Rigidbody>().AddForce(new Vector3(0, 2, -1));
+        bodyPieceClone.GetComponent<Rigidbody>().AddForce(new Vector3(0, 1, -0.2f));
         Destroy(bodyPieceClone, 3f);
     }
 
