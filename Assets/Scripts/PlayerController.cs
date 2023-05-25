e9ffbf82dda8d35502308b585b770bbb474cfc85
Huseyin <akcali-1997@hotmail.com>
2023-05-25 23:26:09 +0300

    cm

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,7 +11,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float rotateBound = 0.5f;
     [SerializeField] float rotateValue = 0;
     [SerializeField] float temp = 0;
-    private float minSize = 0.25f;
+    private float minSize = 0.05f;
+    private float minSizeTorso = 0.5f;
     private bool canMove = false;
     private Touch theTouch;
     #region 
@@ -25,6 +26,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private GameObject hips;
 
     [SerializeField] private GameObject bodyPiece;
+
+    private bool isNearToDead = false;
     private void FixedUpdate()
     {
         Movement();
@@ -88,55 +91,63 @@ public class PlayerController : MonoBehaviour
     public void GetFatOrSlim(float value)
     {
         value = value / 100;
-        if (bodyParts[0].transform.localScale.x  ==  minSize)
-        {
 
+        if (bodyParts[0].transform.localScale.x + value < minSize)
+        {
+            print("1");
             foreach (GameObject obj in bodyParts)
             {
-                obj.transform.DOScale(new Vector3(0,0,0), 1f);
+                obj.transform.DOScale(new Vector3(minSize, obj.transform.localScale.y, minSize), 1f);
             }
-            hips.transform.DOScale( new Vector3(0,0,0), 1f).OnComplete(()=>{ print("Game Over"); });
-
+            hips.transform.DOScale(new Vector3(minSize * 2, hips.transform.localScale.y, minSize * 2), 1f);
         }
-        else if (bodyParts[0].transform.localScale.x + value < minSize)
+        else if (bodyParts[0].transform.localScale.x == minSize)
         {
+            print("2");
             foreach (GameObject obj in bodyParts)
             {
-                obj.transform.DOScale(new Vector3(minSize, 0, minSize), 1f);
+                obj.transform.DOScale(Vector3.zero, 1f);
             }
-            hips.transform.DOScale(hips.transform.localScale + new Vector3(minSize * 0.5f, minSize * 0.5f, minSize), 1f);
+            hips.transform.DOScale(Vector3.zero, 1f).OnComplete(() => { print("Game Over"); });
         }
         else
         {
+            print("3");
             foreach (GameObject obj in bodyParts)
             {
                 obj.transform.DOScale(obj.transform.localScale + new Vector3(value, 0, value), 1f);
             }
-            hips.transform.DOScale(hips.transform.localScale + new Vector3(value * 0.5f, value * 0.5f, value), 1f);
+            hips.transform.DOScale(hips.transform.localScale + new Vector3(value, value * 0.5f, value), 1f);
         }
     }
 
-    public void GetTallOrShort(float value)
+    public bool GetTallOrShort(float value)
     {
         value = value / 100;
-        if (torso.transform.position.y + value <= 0)
+        if (torso.transform.localScale.y + value <= minSizeTorso)
         {
             GetFatOrSlim(value * 100);
+            return false;
         }
         else
         {
-            torso.transform.DOMoveY(torso.transform.position.y + value, 1f);
+            torso.transform.DOScaleY(torso.transform.localScale.y + value, 1f);
             upBody.transform.DOMoveY(upBody.transform.position.y + value, 1f);
+            return true;
         }
     }
 
     public void Hit(Transform hitPoint)
     {
-        GetTallOrShort(-50);
-        Vector3 spawnPoint = new Vector3(transform.position.x, hitPoint.position.y, transform.position.z);
-        GameObject bodyPieceClone = Instantiate(bodyPiece, spawnPoint, Quaternion.identity);
-        bodyPieceClone.GetComponent<Rigidbody>().AddForce(new Vector3(0, 1, -0.2f));
-        Destroy(bodyPieceClone, 3f);
+        if (GetTallOrShort(-25))
+        {
+            Vector3 spawnPoint = new Vector3(transform.position.x, hitPoint.position.y, transform.position.z);
+            GameObject bodyPieceClone = Instantiate(bodyPiece, spawnPoint, Quaternion.identity);
+            Rigidbody pieceRb = bodyPieceClone.GetComponent<Rigidbody>();
+            pieceRb.AddForce(new Vector3(0, 1, -0.05f));
+            pieceRb.AddTorque(Random.insideUnitSphere.normalized * 100);
+            Destroy(bodyPieceClone, 3f);
+        }
     }
 
 }
