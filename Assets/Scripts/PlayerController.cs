977bcf60a7df3e33b7785d927b991a4625d66a7b
Kübra <k.yildirim1499@gmail.com>
2023-05-30 23:00:09 +0300

    level 2 design finished, obstacle animation, die control

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -127,6 +127,7 @@ public class PlayerController : MonoBehaviour
                     head.transform.parent = null;
                     canMove = false;
                     print("Game Over");
+                    DOTween.KillAll();
                 });
             }
             else
